699ce25868ede7a3897a1e11c2aa5c9d90285c2b
David Rothschild <david@rothschildsolutions.com>
2016-07-20 15:56:09 -0700

    add map to location

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,18 +1,44 @@
+$( window).load(
+  function(){
+  setLocation();
+  })
+
 function setLocation() {
   if (navigator.geolocation){
   var timeoutVal = 10 * 1000 * 1000;
   navigator.geolocation.getCurrentPosition (
-    setForm,
+    setFormAndMap,
     displayError,
     { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
     );
   }
 }
 
-function setForm(position){
-    console.log (position);
-    $("#lat").val(position.coords.latitude);
-    $("#longit").val(position.coords.longitude);
+
+
+function setFormAndMap(position){
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    console.log (lat);
+    var pos = new google.maps.LatLng(lat, lng);
+    var options = {
+        zoom: 15,
+        center: pos,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+    var map = new google.maps.Map(document.getElementById("map"), options);
+
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: "User location"
+      });
+
+
+    $("#lat").val(lat);
+    $("#longit").val(lng);
+
+
 }
 
 function displayError(error) {
