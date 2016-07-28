1717fe6a9fca17f62c70d0ad45082502fb07077c
David Rothschild <david@rothschildsolutions.com>
2016-07-28 11:29:50 -0700

    require ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,11 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  #Require SSL
+
+  config.force_ssl = true
+
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
