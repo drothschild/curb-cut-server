8145d6b22d86d7c6b66467658b10cd231ea9650e
David Rothschild <david@rothschildsolutions.com>
2016-08-04 18:46:04 -0700

    add comments to map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,14 +1,16 @@
+// Get current user location
 function getLocation() {
   if (navigator.geolocation){
   var timeoutVal = 10 * 1000 * 1000;
   navigator.geolocation.getCurrentPosition (
+    // Upon success set the map center
     setMapCenter,
     displayError,
     { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
     );
   }
 }
-
+// Convert a geolocation position to google maps LatLng, and center the google map on it.  
 function setMapCenter(position){
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
@@ -17,34 +19,37 @@ function setMapCenter(position){
  }
 
 
-
+// Display error on failure of geo location.
 function displayError(error) {
   var errors = { 
-    1: 'Permission denied',
+    1: 'Geolocation Permission denied',
     2: 'Position unavailable',
     3: 'Request timeout'
   };
   alert("Error: " + errors[error.code]);
 }
 
-
+// Find the bounds the current map
+// Currently unused
 function getCurbsinBounds() {
   var bounds = hanlder.getBounds();
-  var NorthEastCorner = [bounds.getNorthEast.lat(),bounds.getNorthEast.lng()] ;
+  var NorthEastCorner = bounds.getNorthEast() ;
   var SouthWestCorner = bounds.getSouthWest();
   var center = bounds.getCenter();
 }
-
-
+// Display  marker and new form for new curb
+// Note: this does not create a new curb. 
+//  The Create Curb script is called by the Rails controller- it's in the views/curbs/create.js.erb
 function curbsNew(geocode_information) {
-    
+    // Get position of double click
     position = new google.maps.LatLng(geocode_information.lat,geocode_information.lng);
+    // put "New" marker on map
     curbsNewMarker = new google.maps.Marker({
         position: position, 
         map: handler.map.serviceObject,
         icon: 'https://www.google.com/mapfiles/marker_green.png',
     });
-    
+    // request new curb form from rails controller
     $.ajax({
         url: '/curbs/new?' + jQuery.param({curb:geocode_information}) + '#chunked=true',
         type: 'GET',
@@ -54,7 +59,7 @@ function curbsNew(geocode_information) {
                 
                 // Open new form                    
                 openInfowindow(html, curbsNewMarker);
-                // Add close infowindow behavior
+                // Add close infowindow behavior to remove the new marker.
                 google.maps.event.addListener(handler.map.visibleInfoWindow,'closeclick', function(){
                    clearMarker(curbsNewMarker);
                 });   
@@ -72,11 +77,15 @@ function curbsNew(geocode_information) {
 }
 
 var saveCurb = function(event) {
-
+  // runs on clicking the save button in the New Curb form. 
+  // Unused because attachments don't work with ajax requests
+  //  The Create Curb script is called by the Rails controller- it's in the views/curbs/create.js.erb
 }
 
+// Deletes a curb from the server and removes it from the map
 function deleteCurb(event) {
   event.preventDefault();
+  // Create ajax request
   var uri = event.currentTarget.href;
   var data = ""; 
   var ajaxRequest = $.ajax({
@@ -84,20 +93,29 @@ function deleteCurb(event) {
     type: 'delete',
     data: data
   });
+
   ajaxRequest.done(function(serverResponse, status, jqXHR)
   {
+    // Remove the marker from the map
     clearMarker(filterMarker(serverResponse.id));
+    // Filter out the marker from the array stored in handler. 
     handler.markers= handler.markers.filter(function(m){
       return m.serviceObject.id != serverResponse.id;
     });
   })
 }
 
+
+// Loads the edit curb form 
+// Note: The Update Curb script is called by the Rails controller- it's in the views/curbs/update.js.erb
 function editCurb(event) {
   event.preventDefault();
+  // Create the ajax request for the edit form
   var uri = event.currentTarget.href + "/edit";
   var id = event.currentTarget.id;
+  var marker = filterMarker(id);
   var data = ""; 
+  // Store the current content in case the user cancels the update
   var oldContent = handler.currentInfowindow().getContent();
   var ajaxRequest = $.ajax({
     url: uri,
@@ -106,22 +124,24 @@ function editCurb(event) {
   });
   ajaxRequest.done(function(serverResponse, status, jqXHR)
   {
-    var marker = filterMarker(id);
+    // set the currently open infowindow to the edit form   
     handler.currentInfowindow().setContent(serverResponse);
+    // If the user clicks the close button on the info window, revert to the old data
     google.maps.event.addListener(handler.currentInfowindow(),'closeclick', function(){
         handler.currentInfowindow().setContent(oldContent);
     });
   });
 }
 
-
+// Return the marker with the matching id
 function filterMarker(id) {
  return handler.markers.filter(function(m){
     return m.serviceObject.id == id;
   })[0];
 }
 
-
+// Opens an info window
+// (Note: this is not for the windows associated with markers created by gmaps4rails- those are handled by the gem's javascript )
 function openInfowindow(html, marker)  {
   // Close previous infowindow if exists
   closeInfowindow();
@@ -131,6 +151,7 @@ function openInfowindow(html, marker)  {
 
 }
 
+// Close current infowindow on the screen.
 function closeInfowindow() {
    if (handler.currentInfowindow()){
         handler.currentInfowindow().close();
@@ -138,7 +159,7 @@ function closeInfowindow() {
 
 }    
 
-
+// Clears the a marker from the map
 function clearMarker(marker) {
     try {
         marker.setMap(null); 
@@ -147,8 +168,8 @@ function clearMarker(marker) {
     }
 }
 
-
-
+// Uses the geocoder gem to get the address information based on the latlng 
+// Then exports it in a callback
 function geocodePoint(latLng, callback) {
     var street_number = ''; 
     var street = ''; 
@@ -162,6 +183,7 @@ function geocodePoint(latLng, callback) {
       
     geocoder.geocode({'latLng': latLng }, function(responses) {  
         if (responses && responses.length > 0) {
+          // Extract the full address
           name = 'Curb at ' + responses[0].formatted_address;
               // Extract address parts
               responses[0].address_components.forEach(function(el) { 
@@ -192,7 +214,7 @@ function geocodePoint(latLng, callback) {
               
         }
         else {
-            // Inform user
+            // Inform user on failure
             alert_user("Google Maps had some trouble finding the position, try somewhere else", "alert-error");
         }
     });  
--- app/controllers/curbs_controller.rb
+++ app/controllers/curbs_controller.rb
@@ -1,25 +1,13 @@
 class CurbsController < ApplicationController
+  before_action :set_curb, only: [:show, :edit, :update, :destroy ]
 
   def index
-    # Sometime change all to map bounds
+    # Todo: change all to map bounds
     @curbs = Curb.all
-    img_url = ActionController::Base.helpers.path_to_image('red_wheelchair.png')
-    @hash = Gmaps4rails.build_markers(@curbs) do |curb, marker|
-      marker.infowindow render_to_string(:partial => 'show', :locals => { :@curb => curb})
-
-      marker.lat curb.lat
-      marker.lng curb.lng
-      marker.picture({
-                  :url => img_url,
-                  :width   => 16,
-                  :height  => 16
-                 })
-      marker.json ({id: curb.id})
-    end
+    @hash = curbs_to_hash(@curbs)
   end
 
   def show 
-    @curb = Curb.find(params[:id])
   end
 
   def new
@@ -28,63 +16,30 @@ class CurbsController < ApplicationController
   end
 
   def edit
-    @curb = Curb.find(params[:id])
     render partial: 'form',  :locals => { :@curb => @curb}
   end
 
   def create
-
     curb = Curb.new(curb_params)
-
     respond_to do |format|
       if curb.save
-        img_url = ActionController::Base.helpers.path_to_image('red_wheelchair.png')
-        curbs = [*curb]
-        @hash = Gmaps4rails.build_markers(curbs) do |curb, marker|
-          marker.infowindow render_to_string(:partial => 'show', :locals => { :@curb => curb})
-
-            marker.lat curb.lat
-            marker.lng curb.lng
-            marker.picture({
-                      :url => img_url,
-                      :width   => 16,
-                      :height  => 16
-                     })
-            marker.json ({id: curb.id})
-          end
-
+        @hash = curbs_to_hash([*curb])
         format.js
       end
     end
   end
 
   def update
-    @curb = Curb.find(params[:id])
     respond_to do |format| 
       if @curb.update(curb_params)
-        curbs = [*@curb]
-        img_url = ActionController::Base.helpers.path_to_image('red_wheelchair.png')
-        @hash = Gmaps4rails.build_markers(curbs) do |curb, marker|
-          marker.infowindow render_to_string(:partial => 'show', :locals => { :@curb => curb})
-          marker.lat curb.lat
-          marker.lng curb.lng
-          marker.picture({
-                    :url => img_url,
-                    :width   => 16,
-                    :height  => 16
-                   })
-          marker.json ({id: curb.id})
-        end
+        @hash = curbs_to_hash([*@curb])
         format.js
-      else
-        # Write something here.
       end
     end
   end
 
 
   def destroy
-    @curb = Curb.find(params[:id])
     @curb.destroy
     if request.xhr?
       render json: {id: params[:id]}
@@ -98,6 +53,27 @@ class CurbsController < ApplicationController
 
   def curb_params
     params.require(:curb).permit(:lat, :lng, :photo, :street_number, :street, :zip, :city, :state, :country, :name, :delete_image)
+  end
+
+  def curbs_to_hash(curbs)
+    img_url = ActionController::Base.helpers.path_to_image('red_wheelchair.png')
+    hash = Gmaps4rails.build_markers(curbs) do |curb, marker|
+      marker.infowindow render_to_string(:partial => 'show', :locals => { :@curb => curb})
+
+      marker.lat curb.lat
+      marker.lng curb.lng
+      marker.picture({
+                  :url => img_url,
+                  :width   => 16,
+                  :height  => 16
+                 })
+      marker.json ({id: curb.id})
+    end
+    hash
+  end
+
+  def set_curb
+    @curb = Curb.find(params[:id])
   end
 
 end
